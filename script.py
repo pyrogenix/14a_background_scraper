5469a3c0f9ecb6c2d537562480a5c9f31f59779a
pyrogenix <mach1amh@gmail.comgit config --global user.email mach1amh@gmail.com>
2020-04-24 01:32:53 -0700

    added pdfs

--- script.py
+++ script.py
@@ -5,11 +5,26 @@ import PyPDF2 as p2
 import pdfquery
 import requests
 import unicodedata
+import pdfminer
+import fitz
 from bs4 import BeautifulSoup
 
-for root, dirs, files in os.walk('./14a_pdf', topdown=False):
-    for name in files:
-        print(os.path.join(root, name))
-    for name in dirs:
-        print(os.path.join(root, name))
+#for root, dirs, files in os.walk('./14a_pdf', topdown=False):
+#    for name in files:
+#        print(os.path.join(root, name))
+#    for name in dirs:
+#        print(os.path.join(root, name))
 
+basepath = './14a_pdf'
+for fname in os.listdir(basepath):
+    path = os.path.join(basepath, fname)
+    if os.path.isdir(path):
+        continue
+print(os.path.join(basepath, fname))
+
+print(fitz.__doc__)
+#class Document
+#    __init__(filename='./14a_pdf/2732707020_FAIRPOINT_14A_20170227.pdf')
+#fitz.Document('./14a_pdf/2732707020_FAIRPOINT_14A_20170227.pdf') 
+Document.getToC(simple=True)
+Document.pageCount()
