599782a896af428463572c6068b7efc2234efea7
pyrogenix <mach1amh@gmail.comgit config --global user.email mach1amh@gmail.com>
2020-05-01 01:33:23 -0700

    began regex drafting

--- data_extractor.py
+++ data_extractor.py
@@ -1,8 +1,54 @@
 import re
 import sys
 import xlsxwriter
+import soupsieve
+import urllib.request, urllib.error, urllib.parse
+from xlrd import open_workbook
+from bs4 import BeautifulSoup
 
-file_name = './a2231072zdefm14a.htm'
+#file_name = './a2231072zdefm14a.htm'
+file_name = './a2230778zdefm14a.htm'
+
+# url = 'https://www.sec.gov/Archives/edgar/data/861361/000104746917000512/a2230778zdefm14a.htm'
+
+# book = open_workbook('url.xlsx', on_demand = True)
+# for name in book.sheet_names():
+#     if name.endswith('.htm'):
+#         sheet = book.sheet_by_name(name)
+#         rowIndex = -1
+#         for cell in sheet.col(0): 
+#             if '.htm' in cell.value:
+#                 break
+
+#         if row != -1:
+#             cells = sheet.row(row)
+#             for cell in cells:
+#                 print(cell.value)
+
+#         book.unload_sheet(name) 
+
+# response = urllib.request.urlopen(url)
+# webContent = response.read()
+
+sec_htm = 'a2230778zdefm14a.htm#'
+
+# f = open(sec_htm, 'wb')
+# f.write(webContent)
+# f.close
+
+# filename = urllib.parse.urlsplit(url, scheme='', allow_fragments=True)
+# print(filename[2])
+# print(webContent)
+
+
+
+month = ['July', 'August', 'july', 'august']
+space = [' ', '&nbsp;']
+day = ['7']
+nth = ["", "st", "nd", "rd", "th"]
+year = ['2016']
+#datee = [month[-0:] + space[1] + day[-0:] + ',' + space[0] + year[0]]
+#print(datee)
 
 def search_multiple_strings_in_file(file_name, list_of_strings):
     """Get line from the file along with line numbers, which contains any string from the list"""
@@ -23,12 +69,22 @@ def search_multiple_strings_in_file(file_name, list_of_strings):
     return list_of_results
 
 # search for given strings in the file 'sample.txt'
-matched_lines = search_multiple_strings_in_file(file_name, ['background of the merger', 'termination fee'])
+matched_lines = search_multiple_strings_in_file(file_name, month)
+
 
 print('Total Matched lines : ', len(matched_lines))
 for elem in matched_lines:
     # sys.stdout = open('file.htm','wt')
     print('Word = ', elem[0], ' :: Line Number = ', elem[1], ' :: Line = ', elem[2])
+
+linenum = 0
+with open(file_name, 'r') as txtt:
+    for line in txtt:
+        linenum +=1
+        pattern = re.compile(r'\d')
+        matches = pattern.finditer(str(txtt))
+        for match in matches:
+            print(match)
 
 # excel
 workbook = xlsxwriter.Workbook('./output.xlsx')
@@ -49,3 +105,6 @@ for elem in matched_lines[:50]:
     column += 1
 
 workbook.close()
+
+if any(line in file_name for line in month):
+    print(file_name)
